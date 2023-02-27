5f3cc26b8070e0bdad03593347da53655ccfc00e
jojimjohn <philijoji@gmail.com>
2023-02-28 00:02:49 +0800

    Update app.js
    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const corsOptions ={
   origin:'*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials:true,            //access-control-allow-credentials:true
+  credentials:true,           
   optionSuccessStatus:200,
 }
 
