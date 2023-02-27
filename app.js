b2d698dd8770e7e491ba10aaaedc31151ff5fe9e
jojimjohn <philijoji@gmail.com>
2023-02-27 17:17:36 +0800

    Update app.js
    changes to cors

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
+
 
 // Middleware for parsing JSON
 app.use(express.json());
